4f65e8246ce49c4ecc8b03501c3e5335427ef7ce
Davit <57663927+DavitV-yan@users.noreply.github.com>
2021-12-09 13:24:06 +0400

    Update logInPage.js
    Optimized code

--- integration/pages/logInPage.js
+++ integration/pages/logInPage.js
@@ -1,15 +1,13 @@
 import { currancyType } from "./homePage";
-const URL = "http://spv2-stage.betcoapps.com/";
-const signInButton =
-  "#root > div.Popup-fullPagePopupWrapperStyles-0-2-49.Popup-fullPagePopupWrapperStyles-d8-0-2-73 > div.popupContainerStyles > div.Popup-popupChildrenStyles-0-2-51.Popup-popupChildrenStyles-d9-0-2-74 > div > form > button > div";
+const URL = "";
+const signInButton = 'sign_in'
 const logOutTitle = '[data-testid="logout"]';
 const errorMessage = '#root > div.Popup-fullPagePopupWrapperStyles-0-2-35.Popup-fullPagePopupWrapperStyles-d8-0-2-59 > div.popupContainerStyles > div.Popup-popupChildrenStyles-0-2-37.Popup-popupChildrenStyles-d9-0-2-60 > div > form > div:nth-child(1) > div.Input-fieldMessageStyles-0-2-426.Input-fieldErrorMessageStyles-0-2-430 > p';
 
 
 class LogInPage {
   clickOnSigninButton() {
-    const button = cy.get(signInButton);
-    button.click();
+    cy.get(signInButton).click();
   }
 
   fillEmail(value) {
@@ -36,31 +34,3 @@ class LogInPage {
     cy.get(logOutTitle).click();
   }
 }
-
-//     getPasswordError() {
-//       return cy.get(`[data-testid=SignInPasswordError]`);
-//     }
-
-//     fillEmail(value) {
-//       const field = cy.get(`[data-testid=SignInEmailField]`);
-//       field.clear();
-//       field.type(value);
-
-//       return this;
-//     }
-
-//     fillPassword(value) {
-//       const field = cy.get(`[data-testid=SignInPasswordField]`);
-//       field.clear();
-//       field.type(value);
-
-//       return this;
-//     }
-
-//     submit() {
-//       const button = cy.get(`[data-testid=SignInSubmitButton]`);
-//       button.click();
-//     }
-//   }
-
-export default LogInPage;
