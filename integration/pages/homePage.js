0e5022f71dfdce53672a870d3d68d6cd99fc69a1
Davit <57663927+DavitV-yan@users.noreply.github.com>
2021-12-09 13:20:08 +0400

    Update homePage.js
    Changed the routes locators

--- integration/pages/homePage.js
+++ integration/pages/homePage.js
@@ -1,42 +1,41 @@
-const sportRout = "#root > div > div > nav > a:nth-child(1)";
-const liveRout = "#root > div > div > nav > a:nth-child(2)";
-const esportsRout = "#root > div > div > nav > a:nth-child(3)";
-const casinoRout = "#root > div > div > nav > a:nth-child(4)";
-const liveCasinoRout = "#root > div > div > nav > a:nth-child(5)";
-const gamesRout = "#root > div > div > nav > a:nth-child(6)";
-const promotionRout = "#root > div > div > nav > a:nth-child(7)";
-const virtualSportsRout = "#root > div > div > nav > a:nth-child(8)";
+const sportRout = "Sport";
+const liveRout = "In play";
+const esportsRout = "Esport";
+const casinoRout = "Casino";
+const liveCasinoRout = "Live Casino";
+const gamesRout = "Games";
+const promotionRout = "Promotions";
+const virtualSportsRout = "Virtual Sports";
 export const currancyType = "[data-testid='balance-currency']";
-export const logInButton =
-  "#root > div > div > header > div.Header-rightSideStyles-0-2-9.Header-leftAndRightSides-0-2-4 > button";
+export const logInButton = "header_login";
 
 class HomePage {
   clickOnSportRout() {
-    cy.get(sportRout).click();
+    cy.contains(sportRout).click();
   }
   clickOnLiveRout() {
-    cy.get(liveRout).click();
+    cy.contains(liveRout).click();
   }
   clickOnEsportRout() {
-    cy.get(esportsRout).click();
+    cy.contains(esportsRout).click();
   }
   clickOnCasinoRout() {
-    cy.get(casinoRout).click();
+    cy.contains(casinoRout).click();
   }
   clickOnLiveCasinoRout() {
-    cy.get(liveCasinoRout).click();
+    cy.contains(liveCasinoRout).click();
   }
   clickOnGamesRout() {
-    cy.get(gamesRout).click();
+    cy.contains(gamesRout).click();
   }
   clickOnPromotionRout() {
-    cy.get(promotionRout).click();
+    cy.contains(promotionRout).click();
   }
   clickOnVirtualSportsRout() {
-    cy.get(virtualSportsRout).click();
+    cy.contains(virtualSportsRout).click();
   }
   clickOnLoginButton() {
-    cy.get(logInButton).click();
+    cy.contains(logInButton).click();
   }
 }
 
